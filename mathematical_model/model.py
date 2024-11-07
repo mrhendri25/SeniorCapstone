e8a4b28a51dc1270fa37415a85951101fbd2417b
lukad11 <117225714+lukad11@users.noreply.github.com>
2024-11-07 19:18:50 +0000

    getting back

--- mathematical_model/model.py
+++ mathematical_model/model.py
@@ -1,25 +1,18 @@
 import pandas as pd
 import numpy as np
-from determine_points import determine_home_points, determine_away_points
+from determine_points import determine_points
+from define_regression import points_regression
+from pull_data import data_to_train_model, prediction_data, get_schedule
 
 def monte_carlo_simulation(num_simulations):
-    home_wins = 0
-    away_wins = 0
-    draws = 0
-
+    training_df = data_to_train_model()
+    regress = points_regression(training_df)
+    offense_df, defense_df = prediction_data()
+    df_games = get_schedule()
     for _ in range(num_simulations):
-        rng = np.random.default_rng()
-        home_goals = determine_home_points(rng.random())
-        away_goals = determine_away_points(rng.random())
-        
-        if home_goals > away_goals:
-            home_wins += 1
-        elif home_goals < away_goals:
-            away_wins += 1
-        else:
-            draws += 1
+        offense_df, defense_df = determine_points(offense_df, defense_df, df_games, regress)
+    return offense_df, defense_df
 
-    return home_wins, away_wins, draws
-
-x, y, z = monte_carlo_simulation(1000)
-print(x, y, z)
+x, y = monte_carlo_simulation(1)
+print(x)
+print(y)
